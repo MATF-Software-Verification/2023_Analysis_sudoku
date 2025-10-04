4a656eeeb3af6bf5118d3394f359cd070c0177c6
Aleksandar Stoiljković <139758129+Aca99BG@users.noreply.github.com>
2025-10-04 16:47:59 +0200

    Update build_with_coverage.sh

--- lcov/scripts/build_with_coverage.sh
+++ lcov/scripts/build_with_coverage.sh
@@ -8,7 +8,9 @@ cd ../../sudoku
 g++ -fprofile-arcs -ftest-coverage -O0 -g \
     game.cpp solver.cpp altproj.cpp tests.cpp main.cpp \
     -o sudoku_tests --coverage
+    
 # Pokeretanje testova
 ./sudoku_tests
-# Vrati se nazad
+
+# Povratak u /scripts folder
 cd - > /dev/null
